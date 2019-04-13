ed04fe9798c88d0b2c856278cbbe2c3a219d2dc4
cryzereye <cryzereye@github.com>
2019-04-13 10:35:43 +0800

    Exercises 1 and 2

---
+++ exercise2.py
@@ -0,0 +1,7 @@
+# exercise2.py
+
+name = input("What is your name? ")
+age =  int(input("How old are you? "))
+age += 10
+
+print("You are " + name + ". In ten years, you will be " + age + " years old")
